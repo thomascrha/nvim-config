55afbefdea5703ff92a848b89668c2baa554ea72
Thomas Crha <thomas.crha@qbe.com>
2023-02-02 19:34:59 +1100

    added py_lsp for virtual env and lsp support

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -152,5 +152,16 @@ require('lazy').setup({
   -- scroll bar indication
   "petertriho/nvim-scrollbar",
 
+  -- virtualenv support for pyright
+  {
+    'HallerPatrick/py_lsp.nvim',
+    config = function()
+      require("py_lsp").setup {
+        -- This is optional, but allows to create virtual envs from nvim
+        host_python = "/usr/bin/python3",
+        default_venv_name = "venv" -- For local venv
+      }
+    end
+  }
 })
 
